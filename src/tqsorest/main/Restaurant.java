21ba38d6ae1540f720a388f5e46d1e58976d4c8f
Tiago Ferreira <tiagoffcc@hotmail.com>
2014-10-13 20:37:42 +0100

    Added last configs for travis

--- src/tqsorest/main/Restaurant.java
+++ src/tqsorest/main/Restaurant.java
@@ -4,7 +4,10 @@ public class Restaurant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+	}
+	
+	public String printHello() {
+		return "Hello";
 	}
 
 }
