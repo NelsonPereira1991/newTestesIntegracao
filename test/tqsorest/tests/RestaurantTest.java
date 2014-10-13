46f065989f1bdae3793b30a6093dfa530cd5a6af
Tiago Ferreira <tiagoffcc@hotmail.com>
2014-10-13 21:00:43 +0100

    new repo maybe working

---
+++ test/tqsorest/tests/RestaurantTest.java
@@ -0,0 +1,18 @@
+package tqsorest.tests;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tqsorest.main.Restaurant;
+
+public class RestaurantTest {
+
+	@Test
+	public void testRestaurant(){
+		Restaurant restaurant = new Restaurant();
+		assertEquals("A test for HelloString", "Hello", restaurant.printHello());
+	}
+
+}
